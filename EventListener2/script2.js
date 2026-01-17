2c6fca6bff7c76d3c15a9acbf7e083ada410ae8b
YashPathak18 <yashpathak2106@gmail.com>
2026-01-17 20:29:52 +0530

    DOM

---
+++ EventListener2/script2.js
@@ -0,0 +1,16 @@
+let form = document.querySelector("form");
+let inputs = document.querySelector("input");
+
+form.addEventListener("submit",function(dets){
+    dets.preventDefault();
+
+    /*
+    console.log(
+        inputs[0].value,  
+        inputs[1].value,  
+        inputs[2].value,
+        inputs[3].value,
+    )
+    */
+});
+
