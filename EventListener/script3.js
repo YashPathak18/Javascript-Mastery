26eb30b3579435e5da0056f63226aaccad7f6eef
YashPathak18 <yashpathak2106@gmail.com>
2026-01-17 12:37:15 +0530

    window event listener

--- EventListener/script3.js
+++ EventListener/script3.js
@@ -1,3 +1,14 @@
 
 
 let h1=document.querySelector("h1");
+
+window.addEventListener("keydown",function(dets){
+    if(dets.key === " "){
+        h1.textContent ="SPC";
+    }
+    else{
+        h1.textContent=dets.key;
+    }
+});
+
+//
